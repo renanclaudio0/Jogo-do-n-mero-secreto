ade1a545dfef7384209a2fb0d9b9cc5a1e311561
Renan Claudio <renanclaudio0@gmail.com>
2025-10-07 02:48:46 -0400

    Atualizando app.js com as últimas alterações

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto =  gerarNumeroAleatorio();
 let tentativas = 1;
-
+let testeteste
+//teste teste
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector (tag);
     campo.innerHTML = texto;
